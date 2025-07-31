2d0935768814cd43ed95429a94e4681529f4f8a7
Szymon Chmal <szymonchmal@gmail.com>
2025-07-31 17:20:50 +0200

    fix(redux-devtools-plugin): import Redux DevTools CLI dynamically (#13)
    Fixed compatibility issue with @redux-devtools/cli when running in CommonJS environments.

--- packages/redux-devtools-plugin/src/metro.ts
+++ packages/redux-devtools-plugin/src/metro.ts
@@ -1,15 +1,17 @@
 import type { ConfigT as MetroConfig } from 'metro-config';
 import { REDUX_DEVTOOLS_PORT } from './constants';
-import setupWebSocketRelay from '@redux-devtools/cli';
 
 export const withRozeniteReduxDevTools = <
   T extends MetroConfig | Promise<MetroConfig>
 >(
   config: T
 ): T => {
-  setupWebSocketRelay({
-    hostname: 'localhost',
-    port: REDUX_DEVTOOLS_PORT,
+  // This is ESM only, so we need to import it dynamically in case of CJS
+  import('@redux-devtools/cli').then(({ default: setupWebSocketRelay }) => {
+    setupWebSocketRelay({
+      hostname: 'localhost',
+      port: REDUX_DEVTOOLS_PORT,
+    });
   });
 
   return config;
