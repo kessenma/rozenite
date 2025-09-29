88065c2cc18f98a091f77af60ff0b485d3b9e285
Szymon Chmal <szymonchmal@gmail.com>
2025-09-29 11:06:54 +0200

    fix(metro): return async function instead of a promise (#113)
    Metro 0.83.2 no longer supports configs as Promises, so Rozenite must
    provide a workaround by returning an async function instead of a
    Promise. This change restores the ability to bundle JavaScript when
    using React Native 0.81.

--- packages/metro/src/index.ts
+++ packages/metro/src/index.ts
@@ -23,103 +23,105 @@ export type RozeniteMetroConfig<TMetroConfig = unknown> = Omit<
   ) => Promise<TMetroConfig> | TMetroConfig;
 };
 
-export const withRozenite = async <T extends MetroConfig>(
+export const withRozenite = <T extends MetroConfig>(
   config: T | Promise<T>,
   options: RozeniteMetroConfig<T> = {}
-): Promise<T> => {
-  const resolvedConfig = await config;
-  const projectRoot = resolvedConfig.projectRoot ?? process.cwd();
+): () => Promise<T> => {
+  return async () => {
+    const resolvedConfig = await config;
+    const projectRoot = resolvedConfig.projectRoot ?? process.cwd();
 
-  if (options.enabled === undefined) {
-    logger.info(
-      'Rozenite will no longer be enabled by default in the next version.'
-    );
-    logger.info(
-      'To continue using Rozenite, please set `enabled` in the options.'
-    );
-    logger.info('Remember to make it conditional to avoid bundling issues.');
+    if (options.enabled === undefined) {
+      logger.info(
+        'Rozenite will no longer be enabled by default in the next version.'
+      );
+      logger.info(
+        'To continue using Rozenite, please set `enabled` in the options.'
+      );
+      logger.info('Remember to make it conditional to avoid bundling issues.');
 
-    if (isBundling(projectRoot)) {
+      if (isBundling(projectRoot)) {
+        return resolvedConfig;
+      }
+    }
+
+    if (options.enabled === false) {
       return resolvedConfig;
     }
-  }
 
-  if (options.enabled === false) {
-    return resolvedConfig;
-  }
-
-  const { devModePackage, middleware: rozeniteMiddleware } = initializeRozenite(
-    {
-      projectRoot,
-      ...options,
-    }
-  );
-
-  const rozeniteMetroConfig = {
-    ...resolvedConfig,
-    watchFolders: devModePackage
-      ? [...(resolvedConfig.watchFolders ?? []), devModePackage.path]
-      : resolvedConfig.watchFolders,
-    resolver: {
-      ...resolvedConfig.resolver,
-      extraNodeModules: devModePackage
-        ? {
-            ...(resolvedConfig.resolver?.extraNodeModules ?? {}),
-            [devModePackage.name]: require.resolve(devModePackage.name, {
-              paths: [projectRoot],
-            }),
-
-            // Rozenite package should use the same versions of React and React Native as the app.
-            // Using dirname as sometimes developers use deep imports for react-native.
-            react: path.dirname(
-              require.resolve('react', { paths: [projectRoot] })
-            ),
-            'react-native': path.dirname(
-              require.resolve('react-native', {
-                paths: [projectRoot],
-              })
-            ),
-          }
-        : resolvedConfig.resolver?.extraNodeModules,
-      resolveRequest: (context, moduleName, platform) => {
-        // Unfortunately, 'web' doesn't include certain internal modules like 'react-native/Libraries/WebSocket/WebSocketInterceptor'.
-        // This is currently the only module that we need to mock, but it may change in the future.
-        if (
-          platform === 'web' &&
-          moduleName === 'react-native/Libraries/WebSocket/WebSocketInterceptor'
-        ) {
-          return {
-            type: 'empty',
-          };
-        }
-
-        return (
-          resolvedConfig.resolver?.resolveRequest?.(
-            context,
-            moduleName,
-            platform
-          ) ?? context.resolveRequest(context, moduleName, platform)
-        );
-      },
-    },
-    server: {
-      ...resolvedConfig.server,
-      enhanceMiddleware: (metroMiddleware, server) => {
-        const prevMiddleware =
-          resolvedConfig.server?.enhanceMiddleware?.(metroMiddleware, server) ??
-          metroMiddleware;
-
-        return rozeniteMiddleware.use(prevMiddleware);
-      },
-    },
-  } satisfies MetroConfig;
-
-  if (options.enhanceMetroConfig) {
-    const enhancedConfig = await options.enhanceMetroConfig(
-      rozeniteMetroConfig
+    const { devModePackage, middleware: rozeniteMiddleware } = initializeRozenite(
+      {
+        projectRoot,
+        ...options,
+      }
     );
-    return enhancedConfig;
-  }
 
-  return rozeniteMetroConfig;
+    const rozeniteMetroConfig = {
+      ...resolvedConfig,
+      watchFolders: devModePackage
+        ? [...(resolvedConfig.watchFolders ?? []), devModePackage.path]
+        : resolvedConfig.watchFolders,
+      resolver: {
+        ...resolvedConfig.resolver,
+        extraNodeModules: devModePackage
+          ? {
+              ...(resolvedConfig.resolver?.extraNodeModules ?? {}),
+              [devModePackage.name]: require.resolve(devModePackage.name, {
+                paths: [projectRoot],
+              }),
+
+              // Rozenite package should use the same versions of React and React Native as the app.
+              // Using dirname as sometimes developers use deep imports for react-native.
+              react: path.dirname(
+                require.resolve('react', { paths: [projectRoot] })
+              ),
+              'react-native': path.dirname(
+                require.resolve('react-native', {
+                  paths: [projectRoot],
+                })
+              ),
+            }
+          : resolvedConfig.resolver?.extraNodeModules,
+        resolveRequest: (context, moduleName, platform) => {
+          // Unfortunately, 'web' doesn't include certain internal modules like 'react-native/Libraries/WebSocket/WebSocketInterceptor'.
+          // This is currently the only module that we need to mock, but it may change in the future.
+          if (
+            platform === 'web' &&
+            moduleName === 'react-native/Libraries/WebSocket/WebSocketInterceptor'
+          ) {
+            return {
+              type: 'empty',
+            };
+          }
+
+          return (
+            resolvedConfig.resolver?.resolveRequest?.(
+              context,
+              moduleName,
+              platform
+            ) ?? context.resolveRequest(context, moduleName, platform)
+          );
+        },
+      },
+      server: {
+        ...resolvedConfig.server,
+        enhanceMiddleware: (metroMiddleware, server) => {
+          const prevMiddleware =
+            resolvedConfig.server?.enhanceMiddleware?.(metroMiddleware, server) ??
+            metroMiddleware;
+
+          return rozeniteMiddleware.use(prevMiddleware);
+        },
+      },
+    } satisfies MetroConfig;
+
+    if (options.enhanceMetroConfig) {
+      const enhancedConfig = await options.enhanceMetroConfig(
+        rozeniteMetroConfig
+      );
+      return enhancedConfig;
+    }
+
+    return rozeniteMetroConfig;
+  }
 };
